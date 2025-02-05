0b35194206be891de5fd7d6930ad54e39f5a01ca
Jakub <jakub.reniec@pbc.pl>
2025-02-05 22:46:20 +0100

    ch

--- kod.py
+++ kod.py
@@ -133,8 +133,9 @@ for i in selected_tematyki:
                                             
                 wyniki_cal.loc[j, k] = df[(df['tytuł'] == j) & (df['WSKAŹNIK'] == k) & (df['WAVE'].between(selected_miesiace[0], selected_miesiace[-1]))]['WYNIK'].mean()
             else:
-                wyniki.loc[j, k] = max(int(wyniki.loc[j, 'Druk i E-wydania'], (1 - float(df[(df['tytuł'] == j) & (df['WSKAŹNIK']) / 100== 'współczytelnictwo')]['WYNIK'])) * wyniki.loc[j, 'Druk i E-wydania'] + wyniki.loc[j, 'www'])
-                wyniki_cal.loc[j, k] = max(int(wyniki_cal.loc[j, 'Druk i E-wydania'], (1 - float(df[(df['tytuł'] == j) & (df['WSKAŹNIK']) / 100 == 'współczytelnictwo')]['WYNIK'])) * wyniki_cal.loc[j, 'Druk i E-wydania'] + wyniki_cal.loc[j, 'www'])
+                wyniki.loc[j, k] = max(int(wyniki.loc[j, 'Druk i E-wydania']), (1 - float(df.loc[(df['tytuł'] == j) & (df['WSKAŹNIK'] == 'współczytelnictwo'), 'WYNIK'].iloc[0]) / 100) * wyniki.loc[j, 'Druk i E-wydania'] + wyniki.loc[j, 'www']) if not df.loc[(df['tytuł'] == j) & (df['WSKAŹNIK'] == 'współczytelnictwo'), 'WYNIK'].empty else wyniki.loc[j, 'Druk i E-wydania']
+
+                wyniki_cal.loc[j, k] = max(int(wyniki_cal.loc[j, 'Druk i E-wydania']), (1 - float(df.loc[(df['tytuł'] == j) & (df['WSKAŹNIK'] == 'współczytelnictwo'), 'WYNIK'].iloc[0]) / 100) * wyniki_cal.loc[j, 'Druk i E-wydania'] + wyniki_cal.loc[j, 'www']) if not df.loc[(df['tytuł'] == j) & (df['WSKAŹNIK'] == 'współczytelnictwo'), 'WYNIK'].empty else wyniki_cal.loc[j, 'Druk i E-wydania']
 
 
 wyniki = wyniki[wyniki.index.str.contains(wyszukiwarka, case=False, na=False)]
